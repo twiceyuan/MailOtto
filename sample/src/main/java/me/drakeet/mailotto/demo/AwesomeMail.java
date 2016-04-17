1c0f7e6b6af89610ed82de645512817f5a12290a
twiceyuan <twiceyuan@gmail.com>
2016-04-17 23:18:45 +0800

    add a missing class

---
+++ sample/src/main/java/me/drakeet/mailotto/demo/AwesomeMail.java
@@ -0,0 +1,17 @@
+package me.drakeet.mailotto.demo;
+
+/**
+ * Created by twiceYuan on 4/17/16.
+ *
+ * A custom mail
+ */
+public class AwesomeMail {
+
+    String title;
+    String content;
+
+    public AwesomeMail(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
